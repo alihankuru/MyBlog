1d3d7a340fcbc222cef7c0d35a5d5514f8fa1dfc
AK <ali.kuru6@hotmail.com>
2024-03-21 22:21:42 +0300

    Fix

---
+++ MyBlog.BusinessLayer/ValidationrRules/ArticleValidation/CreateArticleValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MyBlog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.BusinessLayer.ValidationrRules.ArticleValidation
+{
+    public class CreateArticleValidation:AbstractValidator<Article>
+    {
+        public CreateArticleValidation()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Bu title alanı boş bırakılmaz").MinimumLength(5).WithMessage("Lütffen en az 5 karakter veri girişi yapınız").MinimumLength(100).WithMessage("Lütfen en fazla 100 karakter veri girişi");
+
+        }
+    }
+}
--- MyBlog.BusinessLayer/ValidationrRules/CategoryValidation/CreateCategoryValidation.cs
+++ MyBlog.BusinessLayer/ValidationrRules/CategoryValidation/CreateCategoryValidation.cs
@@ -12,7 +12,10 @@ namespace MyBlog.BusinessLayer.ValidationrRules.CategoryValidation
     {
         public CreateCategoryValidation()
         {
-            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Bu categoryname alanı boş bırakılmaz");
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Bu categoryname alanı boş bırakılmaz"); 
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen En az 3 karakter veri girişi yapınız");
+            RuleFor(x => x.CategoryName).MaximumLength(3).WithMessage("Lütfen En fazla 30 karakter veri girişi yapınız");
+
         }
     }
 }
