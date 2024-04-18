e9deb53bacdf0697b623a051a636c8035f87eb1b
AK <ali.kuru6@hotmail.com>
2024-04-18 21:00:34 +0300

    -

--- MyBlog.PresentationLayer/Program.cs
+++ MyBlog.PresentationLayer/Program.cs
@@ -46,10 +46,21 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
