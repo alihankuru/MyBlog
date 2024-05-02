e10bc862bc19275938d1ed26c719f8fb750e1598
AK <ali.kuru6@hotmail.com>
2024-05-02 15:48:16 +0300

    Step 10

--- MyBlog.PresentationLayer/ViewComponents/WriterLayoutViewComponents/_PartialUserProfilePartial.cs
+++ MyBlog.PresentationLayer/ViewComponents/WriterLayoutViewComponents/_PartialUserProfilePartial.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBlog.DataAccessLayer.Context;
 using MyBlog.EntityLayer.Concrete;
 
 namespace MyBlog.PresentationLayer.ViewComponents.WriterLayoutViewComponents
@@ -7,10 +9,12 @@ namespace MyBlog.PresentationLayer.ViewComponents.WriterLayoutViewComponents
     public class _PartialUserProfilePartial:ViewComponent
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly BlogContext _context;
 
-        public _PartialUserProfilePartial(UserManager<AppUser> userManager)
+        public _PartialUserProfilePartial(UserManager<AppUser> userManager, BlogContext context)
         {
             _userManager = userManager;
+            _context = context;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
@@ -21,6 +25,10 @@ namespace MyBlog.PresentationLayer.ViewComponents.WriterLayoutViewComponents
             ViewBag.name = userInfo.Name;
             ViewBag.surname = userInfo.Surname;
             ViewBag.image = userInfo.ImageUrl;
+         
+                
+            var blogCount = _context.Articles.Count(b => b.AppUserId == userInfo.Id);
+            ViewBag.blogCount = blogCount;
 
             return View(userInfo);
         }
