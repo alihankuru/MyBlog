80c41bf24d86c416539849bb895cf6cdcba1b755
AK <ali.kuru6@hotmail.com>
2024-05-02 15:32:20 +0300

    Add step-9 404 page

--- MyBlog.PresentationLayer/Program.cs
+++ MyBlog.PresentationLayer/Program.cs
@@ -53,6 +53,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Index", "?code={0}");
+
 app.UseRouting();
 
 app.UseAuthentication();
