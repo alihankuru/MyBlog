f1998d0938dcea01902d278bc24770221f038862
AK <ali.kuru6@hotmail.com>
2024-05-05 16:47:31 +0300

    Fix

---
+++ MyBlog.PresentationLayer/Areas/Admin/Controllers/TagController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+using MyBlog.EntityLayer.Concrete;
+
+namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Tag")]
+    public class TagController : Controller
+    {
+        private readonly ITagService _tagService;
+
+        public TagController(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var values = _tagService.TGetListAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTag()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateTag(Tag tag)
+        {
+            _tagService.TInsert(tag);
+            return RedirectToAction("Index", "tag");
+        }
+    }
+}
